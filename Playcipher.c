2b46099539178af50ffb78e38f2f613379764cae
balaji <113337367+kattabalaji@users.noreply.github.com>
2023-11-02 12:54:52 +0530

    Create Playcipher.c

--- Playcipher.c
+++ Playcipher.c
@@ -1,214 +1,112 @@
-#include<stdio.h>
-#include<string.h>
-#include<ctype.h>
-int removerepeated(int size,int a[]);
-int insertelementat(int position,int a[],int size);
-main()
-{
- int i,j,k,numstr[100],numcipher[100],numkey[100],lenkey,templen,tempkey[100],flag=-1,size,cipherkey[5][5],lennumstr,row1,row2,col1,col2;
- char str[100],key[100];
- printf("Enter a string\n");
- gets(str);
- for(i=0,j=0;i<strlen(str);i++)
- {
-  if(str[i]!=' ')
-  {
-   str[j]=toupper(str[i]);   
-   j++;
-  }
- }
- str[j]='\0';
- printf("Entered String is %s\n",str);
- size=strlen(str);
- for(i=0;i<size;i++)
- {
-  if(str[i]!=' ')
-  numstr[i]=str[i]-'A';
- }
- lennumstr=i;
- printf("Enter the key (Non repeated elements if possible)\n");
- gets(key);
- for(i=0,j=0;i<strlen(key);i++)
- {
-  if(key[i]!=' ')
-  {
-   key[j]=toupper(key[i]);   
-   j++;
-  }
- }
- key[j]='\0';
- printf("%s\n",key);
- //Storing key in terms of ascii
- k=0;
- for(i=0;i<strlen(key)+26;i++)
- {
-  if(i<strlen(key))
-  {
-   if(key[i]=='J')
-   {
-    flag=8;
-    printf("%d",flag);
-   }
-       numkey[i]=key[i]-'A';   
-  }
-  else
-  {
-    if(k!=9 && k!=flag)
-    {
-            numkey[i]=k; 
-      }
-      k++;
-  }
- }
- templen=i;
- lenkey=removerepeated(templen,numkey);
- printf("Entered key converted according to Play Fair Cipher rule\n");
- for(i=0;i<lenkey;i++)
- {
-     printf("%c",numkey[i]+'A');  
- }
- printf("\n");
- k=0;
- for(i=0;i<5;i++)
- {
-  for(j=0;j<5;j++)
-  {
-   cipherkey[i][j]=numkey[k];   
-   k++;
-  }
- }
- printf("Arranged key\n");
- for(i=0;i<5;i++)
- {
-  for(j=0;j<5;j++)
-  {
-   printf("%c ",cipherkey[i][j]+'A');
-  }
-  printf("\n");
- }   
-   for(i=0;i<lennumstr;i+=2)
-   {
-      if(numstr[i]==numstr[i+1])
-      {
-       insertelementat(i+1,numstr,lennumstr);
-       lennumstr++;
-      }
-   }
-   if(lennumstr%2!=0)
-   {
-    insertelementat(lennumstr,numstr,lennumstr);
-    lennumstr++;
-   }
-   printf("Entered String/Message After Processing according to Play fair cipher rule\n");
-   for(i=0;i<lennumstr;i++)
-   {
-    printf("%c",numstr[i]+'A');
-   }
-   for(k=0;k<lennumstr;k+=2)
-   {
-    for(i=0;i<5;i++)
-    {
-     for(j=0;j<5;j++)
-     {
-      if(numstr[k]==cipherkey[i][j])
-      {
-         row1=i;
-         col1=j;       
-      }
-      if(numstr[k+1]==cipherkey[i][j])
-      {
-         row2=i;
-         col2=j;       
-      }     
-     }
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+void generatePlayfairMatrix(char matrix[5][5], const char *key) {
+    int keyLength = strlen(key),i,j,l;
+    char alphabet[] ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    int k = 0;
+    for ( i = 0; i < keyLength; i++) {
+        if (key[i] == 'J' ||  key[i]=='I');
+        int present = 0;
+        for ( j = 0; j < 5; j++) {
+            for ( l = 0; l < 5; l++) {
+                if (matrix[j][l] == key[i]) {
+                    present = 1;
+                    break;
+                }
+            }
+        }
+        if (!present) {
+            matrix[k / 5][k % 5] = key[i];
+            k++;
+        }
     }
-    if(row1==row2)
-    {
-     col1=(col1-1)%5;
-     col2=(col2-1)%5;
-     if(col1<0)
-     {
-      col1=5+col1;
-     }
-     if(col2<0)
-     {
-      col2=5+col2;
-     }
-     numcipher[k]=cipherkey[row1][col1];
-     numcipher[k+1]=cipherkey[row2][col2];
+    for ( i = 0; i < 26; i++) {
+        if (alphabet[i] == 'J')
+            alphabet[i] = 'I'; 
+        int present = 0;
+        for ( j = 0; j < 5; j++) {
+            for ( l = 0; l < 5; l++) {
+                if (matrix[j][l] == alphabet[i]) {
+                    present = 1;
+                    break;
+                }
+            }
+        }
+        if (!present) {
+            matrix[k / 5][k % 5] = alphabet[i];
+            k++;
+        }
     }
-    if(col1==col2)
-    {
-     row1=(row1-1)%5;
-     row2=(row2-1)%5;
-      if(row1<0)
-     {
-      row1=5+row1;
-     }
-     if(row2<0)
-     {
-      row2=5+row2;
-     }
-     numcipher[k]=cipherkey[row1][col1];
-     numcipher[k+1]=cipherkey[row2][col2];
-    }
-    if(row1!=row2&&col1!=col2)
-    {
-     numcipher[k]=cipherkey[row1][col2];
-     numcipher[k+1]=cipherkey[row2][col1];
-    }
-   } 
-   printf("\nCipher Text is\n");
-   for(i=0;i<lennumstr;i++)
-   {
-    if((numcipher[i]+'A')!='X')
-         printf("%c",numcipher[i]+'A'); 
-   }
-   printf("\n"); 
 }
-int removerepeated(int size,int a[])
-{
- int i,j,k;
- for(i=0;i<size;i++)
-  {
- for(j=i+1;j<size;)
- {
-    if(a[i]==a[j])
-    {    
-     for(k=j;k<size;k++)
-     {
-      a[k]=a[k+1];
-     }
-         size--;
+void findPosition(const char matrix[5][5], char ch, int *row, int *col) {
+	int i,j;
+    for ( i = 0; i < 5; i++) {
+        for ( j = 0; j < 5; j++) {
+            if (matrix[i][j] == ch) {
+                *row = i;
+                *col = j;
+                return;
+            }
         }
-    else
-    {
-      j++;
-     } 
- }
- }
-return(size);
+    }
 }
-int insertelementat(int position,int a[],int size)
-{
-       int i,insitem=23,temp[size+1];
-    for(i=0;i<=size;i++)
-        {
-        if(i<position)
-        {
-            temp[i]=a[i];
+void playfairEncrypt(const char matrix[5][5], char plaintext[], char ciphertext[]) {
+    int len = strlen(plaintext);
+    int i = 0;
+
+    while (i < len) {
+        char first = plaintext[i];
+        char second = plaintext[i + 1];
+        if (first == second) {
+            second = 'X';
+            i--;
         }
-        if(i>position)
-        {
-         temp[i]=a[i-1];    
+
+        int row1, col1, row2, col2;
+        findPosition(matrix, first, &row1, &col1);
+        findPosition(matrix, second, &row2, &col2);
+
+        if (row1 == row2) {
+            ciphertext[i] = matrix[row1][(col1 + 1) % 5];
+            ciphertext[i + 1] = matrix[row2][(col2 + 1) % 5];
+        } else if (col1 == col2) {
+            ciphertext[i] = matrix[(row1 + 1) % 5][col1];
+            ciphertext[i + 1] = matrix[(row2 + 1) % 5][col2];
+        } else {
+            ciphertext[i] = matrix[row1][col2];
+            ciphertext[i + 1] = matrix[row2][col1];
         }
-        if(i==position)
-        {
-            temp[i]=insitem;
+
+        i += 2;
+    }
+    ciphertext[i] = '\0';
+}
+
+int main() {
+	int i,j;
+    char key[50];
+    char plaintext[100];
+    char ciphertext[100];
+    char matrix[5][5];
+
+    printf("Enter the keyword (no spaces): ");
+    scanf("%s", key);
+    printf("Enter the plaintext (no spaces): ");
+    scanf("%s", plaintext);
+
+    generatePlayfairMatrix(matrix, key);
+    playfairEncrypt(matrix, plaintext, ciphertext);
+
+    printf("Playfair Matrix:\n");
+    for ( i = 0; i < 5; i++) {
+        for ( j = 0; j < 5; j++) {
+            printf("%c ", matrix[i][j]);
         }
-        }       
-        for(i=0;i<=size;i++)
-        {
-         a[i]=temp[i];
-        }
+        printf("\n");
+    }
+
+    printf("Encrypted Text: %s\n", ciphertext);
+
+    return 0;
 }
