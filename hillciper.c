3fad12af540601aae2e255e96c98d4546daafa6d
balaji <113337367+kattabalaji@users.noreply.github.com>
2023-11-02 21:13:13 +0530

    Create hillciper.c

--- hillciper.c
+++ hillciper.c
@@ -1,25 +1,43 @@
-#include<stdio.h>
-#include<string.h>
-int main(){
-    char msg[] = "cryptography";
-    char key[] = "crypto";
-    int msgLen = strlen(msg), keyLen = strlen(key), i, j;
-    char newKey[msgLen], encryptedMsg[msgLen], decryptedMsg[msgLen];
-    for(i = 0, j = 0; i < msgLen; ++i, ++j){
-        if(j == keyLen)
-            j = 0;
-        newKey[i] = key[j];
+#include <stdio.h>
+#include <string.h>
+
+int en[100][100], m[100][100], msg[100];
+char ms[100];
+
+void getkeymatrix() {
+    printf("Enter the message: ");
+    scanf("%s", ms);
+
+    for (int i = 0; i < strlen(ms); i++) {
+        msg[i] = ms[i] - 65;
     }
-    newKey[i] = '\0';
-    for(i = 0; i < msgLen; ++i)
-        encryptedMsg[i] = ((msg[i] + newKey[i]) % 26) + 'A';
-    encryptedMsg[i] = '\0';
-    for(i = 0; i < msgLen; ++i)
-        decryptedMsg[i] = (((encryptedMsg[i] - newKey[i]) + 26) % 26) + 'A';
-    decryptedMsg[i] = '\0';
-    printf("Original Message: %s", msg);
-    printf("\nKey: %s", key);
-    printf("\nNew Generated Key: %s", newKey);
-    printf("\nEncrypted Message: %s", encryptedMsg);
-    printf("\nDecrypted Message: %s", msg);
+
+    printf("Enter the key matrix:\n");
+    for (int i = 0; i < strlen(ms); i++) {
+        for (int j = 0; j < strlen(ms); j++) {
+            scanf("%d", &m[i][j]);
+        }
+    }
+}
+
+void encryption() {
+    int i, j, k;
+    for (i = 0; i < strlen(ms); i++) {
+        en[i][0] = 0;
+        for (k = 0; k < strlen(ms); k++) {
+            en[i][0] += m[i][k] * msg[k];
+        }
+    }
+    
+    printf("Encrypted message: ");
+    for (i = 0; i < strlen(ms); i++) {
+        printf("%c", (en[i][0] % 26) + 65);
+    }
+    printf("\n");
+}
+
+int main() {
+    getkeymatrix();
+    encryption();
+    return 0;
 }
